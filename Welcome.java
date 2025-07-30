83492ca7c185e8fd007986ffe30f39c2bf0c89a4
it24100312 <it24100312@my.sliit.lk>
2025-07-30 15:09:17 +0530

    Add Welcome.java program

---
+++ Welcome.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Welcome {
+  public static void main(String[] args){
+    Scanner input =  new Scanner(System.in);
+    System.out.print("Enter your first name : ");
+    String firstName=input.nextLine();
+   System.out.print("Enter your last name : ");
+    String lastName=input.nextLine();
+
+   System.out.println("Welcome to the secound year " + firstName + " " + lastName );
+   input.close();
+}
+
+}
